f1debe03a5bfe7b9289eea0e05f659caa8ace9a8
srutis90 <srutis90@gmail.com>
2016-01-12 11:44:18 +0530

    beefed up parsing test sources

--- jsparser/testsrc/example.js
+++ jsparser/testsrc/example.js
@@ -2,7 +2,18 @@ function add(a, b) {
     var sum = a + b;
     return sum;
 }
+
+function arrayUsages(){
+	var arr = []
+	arr.push(1)
+	function something(){
+		console.log("something")
+	}
+	something();
+}
 function main() {
     add(1, 2);
     console.log("called 'add(...)'");
+
+    arrayUsages();
 	}
