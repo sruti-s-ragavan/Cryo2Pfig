979e220bc7f4ad608e7f95a8043ff1f5015fee11
srutis90 <srutis90@gmail.com>
2016-03-18 14:56:26 -0700

    Db fix scripts

---
+++ ManualFixes.sql
@@ -0,0 +1,8 @@
+--D09
+INSERT INTO logger_log values(0, 'c486980a-74df-40a1-b4eb-07ff1c1dff93', '2015-05-21 21:12:00.399000', 'Part activated', 
+'/hexcom/Current/js_v9/checking.js', 'checking.js', '8ea5d9be-d1b5-4319-9def-495bdccb7f51', '0:47:14.179000');
+
+INSERT INTO logger_log values(0, 'c486980a-74df-40a1-b4eb-07ff1c1dff93', '2015-05-21 21:12:00.400000', 'Text selection offset', 
+'/hexcom/Current/js_v9/checking.js', '2580', '8ea5d9be-d1b5-4319-9def-495bdccb7f51', '0:47:14.180000');
+--End of D09
+
