c14d8c2b6d8644bcd128e6153e9e57f2bb324034
srutis90 <srutis90@gmail.com>
2016-03-22 07:21:20 -0700

    C07 corrections

--- ManualFixes.sql
+++ ManualFixes.sql
@@ -1,3 +1,7 @@
+--C07
+UPDATE logger_log where referrer = referrer -1 where action = 'Text selection offset' and referrer > 0;
+--End of C07
+
 --D09
 INSERT INTO logger_log values(0, 'c486980a-74df-40a1-b4eb-07ff1c1dff93', '2015-05-21 21:12:00.399000', 'Part activated', 
  'checking.js', '/hexcom/Current/js_v9/checking.js', '8ea5d9be-d1b5-4319-9def-495bdccb7f51', '0:47:14.179000');
