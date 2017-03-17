09488db79e0e5e35923d20711e62a03507cac7ee
srutis90 <srutis90@gmail.com>
2017-03-17 14:47:15 -0400

    opencv+tesseract

--- opencv-spike/tesseract_text_recog.py
+++ opencv-spike/tesseract_text_recog.py
@@ -1,8 +1,26 @@
-from PIL import Image
+from PIL import Image, ImageEnhance, ImageFilter
 import sys
 import pytesseract
+import cv2
 
+
+# load image
 filename = sys.argv[1]
-print "----: ", sys.argv[1]
-print(pytesseract.image_to_string(Image.open(filename)))
-#print image_to_string(Image.open('test-english.jpg'), lang='eng')
+
+img = Image.open(filename)
+img = img.filter(ImageFilter.SHARPEN)
+enhancer = ImageEnhance.Contrast(img)
+img = enhancer.enhance(10)
+img = img.convert('1')
+img.save('temp.jpg')
+
+im = cv2.imread('temp.jpg',cv2.COLOR_RGB2GRAY)
+# blur image
+im = cv2.blur(im,(4,4))
+# apply a threshold
+im = cv2.threshold(im, 175 , 250, cv2.THRESH_BINARY)
+im = im[1]
+cv2.imwrite('temp2.jpg', im)
+
+text = pytesseract.image_to_string(Image.open('temp2.jpg'))
+print(text)
