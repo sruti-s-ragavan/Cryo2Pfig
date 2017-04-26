75dc9c500ccffe3807acdf0db53db60a42fc9a14
srutis90 <srutis90@gmail.com>
2017-04-25 18:27:00 -0700

    Temp

--- opencv-spike/detect_shapes.py
+++ opencv-spike/detect_shapes.py
@@ -10,26 +10,28 @@ ap.add_argument("-i", "--image", required=True,
 	help="path to the input image")
 args = vars(ap.parse_args())
 
-# load the image and resize it to a smaller factor so that
-# the shapes can be approximated better
 image = cv2.imread(args["image"])
-resized = imutils.resize(image, width=300)
-ratio = image.shape[0] / float(resized.shape[0])
+# resized = imutils.resize(image, width=300)
+# ratio = image.shape[0] / float(resized.shape[0])
 
 # convert the resized image to grayscale, blur it slightly,
 # and threshold it
-gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-#gray = cv2.bilateralFilter(gray, 11, 17, 17)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# filtered = cv2.bilateralFilter(gray, 11, 17, 17)
 edged = cv2.Canny(gray, 70, 350)
-blurred = cv2.GaussianBlur(edged, (5, 5), 0)
-thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+# blurred = cv2.GaussianBlur(edged, (5, 5), 0)
+_,thresh = cv2.threshold(edged, 127, 255, cv2.THRESH_BINARY)
+
+cv2.imshow("Image", thresh)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # find contours in the thresholded image and initialize the
 # shape detector
 # cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 	# cv2.CHAIN_APPROX_SIMPLE)
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_TREE,
-	cv2.CHAIN_APPROX_SIMPLE)
+	cv2.CHAIN_APPROX_NONE)
 
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 sd = ShapeDetector()
@@ -39,19 +41,21 @@ for c in cnts:
 	# compute the center of the contour, then detect the name of the
 	# shape using only the contour
 	M = cv2.moments(c)
-	cX = int((M["m10"] / M["m00"]) * ratio)
-	cY = int((M["m01"] / M["m00"]) * ratio)
+	cX = int((M["m10"] / M["m00"]))
+	cY = int((M["m01"] / M["m00"]))
 	shape = sd.detect(c)
+	if shape=="hexagon":
 
-	# multiply the contour (x, y)-coordinates by the resize ratio,
-	# then draw the contours and the name of the shape on the image
-	c = c.astype("float")
-	c *= ratio
-	c = c.astype("int")
-	cv2.drawContours(image, [c], -1, (0, 0, 0), 2)
-	cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-		0.5, (255, 0, 0), 2)
+		# multiply the contour (x, y)-coordinates by the resize ratio,
+		# then draw the contours and the name of the shape on the image
+		c = c.astype("float")
+		# c *= ratio
+		c = c.astype("int")
+		cv2.drawContours(image, [c], -1, (0, 0, 0), 2)
 
-	# show the output image
-	cv2.imshow("Image", image)
-	cv2.waitKey(0)
+		cv2.putText(image, shape, (cX, cY+200), cv2.FONT_HERSHEY_SIMPLEX,
+			0.5, (255, 0, 0), 2)
+
+		# show the output image
+		cv2.imshow("Image", image)
+		cv2.waitKey(0)
--- opencv-spike/tesseract_text_recog.py
+++ opencv-spike/tesseract_text_recog.py
@@ -15,12 +15,16 @@ img = img.convert('1')
 img.save('temp.jpg')
 
 im = cv2.imread('temp.jpg',cv2.COLOR_RGB2GRAY)
-# blur image
+
 im = cv2.blur(im,(4,4))
-# apply a threshold
-im = cv2.threshold(im, 175 , 250, cv2.THRESH_BINARY)
-im = im[1]
+
+_, im = cv2.threshold(im, 200 , 200, cv2.THRESH_BINARY_INV)
+cv2.imshow('gray', im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 cv2.imwrite('temp2.jpg', im)
+
 
 text = pytesseract.image_to_string(Image.open('temp2.jpg'))
 print(text)
